652af2549d1915586df91966094553e958813862
GeoTsinis <geo.tsinis@gmail.com>
2023-02-04 17:30:36 +0200

    + calendar page + calendar functionality with fullcalendar

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Form from "./screens/form";
 // import Pie from "./screens/pie";
 // import FAQ from "./screens/faq";
 // import Geography from "./screens/geography";
-// import Calendar from "./screens/calendar";
+import Calendar from "./screens/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -34,12 +34,12 @@ function App() {
               <Route path='/contacts' element= {<Contacts />} />
               <Route path='/invoices' element= {<Invoices />} />
               <Route path='/form' element= {<Form />} />
-              {/* <Route path='/bar' element= {<Bar />} /> */}
+              <Route path='/calendar' element= {<Calendar />} />{/* <Route path='/bar' element= {<Bar />} /> */}
               {/* <Route path='/pie' element= {<Pie />} /> */}
               {/* <Route path='/line' element= {<Line />} /> */}
               {/* <Route path='/faq' element= {<FAQ />} /> */}
               {/* <Route path='/geography' element= {<Geography />} /> */}
-              {/* <Route path='/calendar' element= {<Calendar />} /> */}
+              
             </Routes>
           </main>
         </div>   
